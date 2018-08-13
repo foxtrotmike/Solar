2941fdcd5ec9feb595f3efd21186d126f1aa4ee2
Fayyaz ul Amir Afsar Minhas <foxtrotmike@users.noreply.github.com>
2018-08-13 16:43:45 +0500

    Add files via upload

---
+++ solar.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# Multibody simulation of the solar system
+import math
+from turtle import *
+
+# The gravitational constant G
+G = 6.67428e-11
+
+# Assumed scale: 100 pixels = 1AU.
+AU = (149.6e6 * 1000)     # 149.6 million km, in meters.
+SCALE = 20 / AU
+
+class Body(Turtle):
+    """Subclass of Turtle representing a gravitationally-acting body.
+
+    Extra attributes:
+    mass : mass in kg
+    vx, vy: x, y velocities in m/s
+    px, py: x, y positions in m
+    """
+    
+    name = 'Body'
+    mass = None
+    vx = vy = 0.0
+    px = py = 0.0
+    
+    def attraction(self, other):
+        """(Body): (fx, fy)
+
+        Returns the force exerted upon this body by the other body.
+        """
+        # Report an error if the other object is the same as this one.
+        if self is other:
+            raise ValueError("Attraction of object %r to itself requested"
+                             % self.name)
+
+        # Compute the distance of the other body.
+        sx, sy = self.px, self.py
+        ox, oy = other.px, other.py
+        dx = (ox-sx)
+        dy = (oy-sy)
+        d = math.sqrt(dx**2 + dy**2)
+
+        # Report an error if the distance is zero; otherwise we'll
+        # get a ZeroDivisionError exception further down.
+        if d == 0:
+            raise ValueError("Collision between objects %r and %r"
+                             % (self.name, other.name))
+
+        # Compute the force of attraction
+        f = G * self.mass * other.mass / (d**2)
+
+        # Compute the direction of the force.
+        theta = math.atan2(dy, dx)
+        fx = math.cos(theta) * f
+        fy = math.sin(theta) * f
+        return fx, fy
+
+def update_info(step, bodies):
+    """(int, [Body])
+    
+    Displays information about the status of the simulation.
+    """
+    print('Step #{}'.format(step))
+    for body in bodies:
+        s = '{:<8}  Pos.={:>6.2f} {:>6.2f} Vel.={:>10.3f} {:>10.3f}'.format(
+            body.name, body.px/AU, body.py/AU, body.vx, body.vy)
+        print(s)
+    print()
+
+def loop(bodies):
+    """([Body])
+
+    Never returns; loops through the simulation, updating the
+    positions of all the provided bodies.
+    """
+    timestep = 7*24*3600  # One week
+    
+    for body in bodies:
+        body.penup()
+        body.hideturtle()
+
+    step = 1
+    while True:
+        update_info(step, bodies)
+        step += 1
+
+        force = {}
+        for body in bodies:
+            # Add up all of the forces exerted on 'body'.
+            total_fx = total_fy = 0.0
+            for other in bodies:
+                # Don't calculate the body's attraction to itself
+                if body is other:
+                    continue
+                fx, fy = body.attraction(other)
+                total_fx += fx
+                total_fy += fy
+
+            # Record the total force exerted.
+            force[body] = (total_fx, total_fy)
+
+        # Update velocities based upon on the force.
+        for body in bodies:
+            fx, fy = force[body]
+            body.vx += fx / body.mass * timestep
+            body.vy += fy / body.mass * timestep
+
+            # Update positions
+            body.px += body.vx * timestep
+            body.py += body.vy * timestep
+            body.goto(body.px*SCALE, body.py*SCALE)
+            body.dot(3)
+
+if __name__ == '__main__':
+    #parameters taken from: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+    names = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]#, "pluto"]
+    mass = [0.330,4.87,5.97,0.642,1898,568,86.8,102,0.0146]
+    dis = [57.9,108.2,149.6,227.9,778.6,1433.5,2872.5,4495.1,5906.4]
+    vel = [47.4,35.0,29.8,24.1,13.1,9.7,6.8,5.4,4.7]
+    col = ['red','green','blue']*3
+    
+    sun = Body()
+    sun.name = 'Sun'
+    sun.mass = 1.98892 * 10**30
+    sun.pencolor('yellow')
+    B = [sun]    
+    for i in range(len(names)):
+        b = Body()
+        b.name=names[i]
+        b.mass = mass[i]*10**24
+        b.px = dis[i]*10**9
+        b.vy = vel[i]*1000
+        b.pencolor(col[i])
+        B.append(b)
+        
+    loop(B)
